33fe364dbb2de5a1439e4d1b4bb0de221bdb4e0c
Boba Fett <thomascupchurch@gmail.com>
2021-08-23 15:56:37 -0500

    connect epress with mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,20 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'WalkerUp1*',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 
 
 
